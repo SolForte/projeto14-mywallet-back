ab2b2114610b4453a461949875bfc2328269767e
SolForte <mateusbrandao1234@gmail.com>
2023-04-23 04:30:27 -0300

    Folder preparation

--- src/app.js
+++ src/app.js
@@ -1,19 +1,28 @@
-import express from "express"
-import cors from "cors"
-import { MongoClient } from "mongodb"
-import dotenv from "dotenv"
+import express from "express";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
-dotenv.config()
+app.use(express.json());
+app.use(cors());
 
-let db
-const mongoClient = new MongoClient(process.env.DATABASE_URL)
-mongoClient.connect()
-    .then(() => db = mongoClient.db())
-    .catch((err) => console.log(err.message))
+/*
+Front end routes:
 
-const PORT = 5000
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+Route: / = signinpage
+Route: /cadastro = signuppage
+Route: /home = homepage 
+Route: /nova-transacao/:tipo = transactionpage
+
+Front end database interactions:
+
+Sign-up (cadastro)
+Sign-in (login)
+Transação (depositar, remover)
+Listagem (historico de transação)
+Logout
+
+*/
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
