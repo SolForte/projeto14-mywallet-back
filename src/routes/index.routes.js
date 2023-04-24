af90af0f4d5e4ee17ef7994936c6dc45da8eac3c
SolForte <mateusbrandao1234@gmail.com>
2023-04-24 05:08:35 -0300

    Add post/get transacao to routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import userRouter from "./user.routes.js";
+import transactionRouter from "./transactions.routes.js";
 
 const router = Router();
 router.use(userRouter);
+router.use(transactionRouter);
 
 export default router;
